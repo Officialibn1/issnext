81687b5ed9119126845a81ff584253355a2cb067
ibn <officialibn001@gmail.com>
2023-09-16 11:43:53 +0100

    Restarting practice

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
 
-const Home = () => {
+const HomePage = () => {
   return (
     <div>
-        <div className="noteCard">
-            <h1>Home Page</h1>
-            <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ipsa natus deserunt a, temporibus libero, ex dignissimos aspernatur optio vero incidunt nesciunt at illum maiores quia debitis odit! Quis dolorem quas dolores, adipisci debitis sit illum quisquam nulla culpa molestias nihil minima quasi similique at.
-            </p>
-        </div>
+        <h1>Home Page</h1>
     </div>
   )
 }
 
-export default Home
+export default HomePage
