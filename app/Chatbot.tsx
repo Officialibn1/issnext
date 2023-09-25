8f8b5d33fcf8c650d9c337a069e80320a2cb5467
ibn <officialibn001@gmail.com>
2023-09-25 22:55:16 +0100

    New Integration

--- app/Chatbot.tsx
+++ app/Chatbot.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useEffect } from 'react'
- 
+
 const Chatbot = () => {
   useEffect(() => {
     const script = document.createElement('script')
@@ -9,23 +9,21 @@ const Chatbot = () => {
     document.body.appendChild(script)
  
     script.onload = () => {
-        window.botpressWebChat.init({
-            "composerPlaceholder": "Chat with our AI Customer Assistant",
-            "botConversationDescription": "ISS Chatbot, Created by Ibn.",
-            "botId": "5b9adf49-934c-49b1-b05b-91b3d2d9fd8f",
-            "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
-            "messagingUrl": "https://messaging.botpress.cloud",
-            "clientId": "5b9adf49-934c-49b1-b05b-91b3d2d9fd8f",
-            "lazySocket": true,
-            "botName": "ISS AI Assistant ",
-            "avatarUrl": "https://iss-one.vercel.app/img/chatbot.png",
-            "phoneNumber": "+2349038880282",
-            "emailAddress": "officialibn001@gmail.com",
-            "website": "https://www.linkedin.com/in/isah-ibn-muhammad-5046b125a/",
-            "stylesheet": "https://webchat-styler-css.botpress.app/prod/code/23e24614-43a9-4a78-ab87-9a5ff892498b/v68715/style.css",
-            "frontendVersion": "v1",
-            "useSessionStorage": true
-        });.
+      window.botpressWebChat.init({
+        botId: '5b9adf49-934c-49b1-b05b-91b3d2d9fd8f',
+        hostUrl: 'https://cdn.botpress.cloud/webchat/v1',
+        messagingUrl: 'https://messaging.botpress.cloud',
+        clientId: '5b9adf49-934c-49b1-b05b-91b3d2d9fd8f',
+        botName: "ISS AI Assistant",
+        composerPlaceholder: "Chat with our AI Customer Assistant",
+        botConversationDescription: "ISS Chatbot, Created by Ibn.",
+        avatarUrl: "https://iss-one.vercel.app/img/chatbot.png",
+        phoneNumber: "+2349038880282",
+        emailAddress: "officialibn001@gmail.com",
+        frontendVersion: "v1",
+        useSessionStorage: false,
+        stylesheet: 'https://webchat-styler-css.botpress.app/prod/code/23e24614-43a9-4a78-ab87-9a5ff892498b/v68715/style.css'
+      })
     }
   }, [])
  
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,40 @@
 import '../styles/Globals.scss';
 import NavBar from './Navbar';
-import { Roboto } from "next/font/google";
+declare global {
+  interface Webchat {
+    init: (options: any, id?: string) => void
+    sendEvent: (event: any, id?: string) => void
+    onEvent: (
+      callback: (event: WebchatEvent) => void,
+      topics: WebchatEventType[],
+      id?: string
+    ) => void
+  }
+
+  type WebchatEventType =
+    | "LIFECYCLE.LOADED"
+    | "LIFECYCLE.READY"
+    | "UI.OPENED"
+    | "UI.CLOSED"
+    | "UI.RESIZE" // is this necessary ?
+    | "UI.SET-CLASS" // is this necessary ?
+    | "CONFIG.SET"
+    | "MESSAGE.SENT"
+    | "MESSAGE.RECEIVED"
+    | "MESSAGE.SELECTED"
+    | "USER.CONNECTED"
+
+  interface WebchatEvent {
+    type: WebchatEventType
+    value: any
+    chatId: string
+  }
+
+  interface Window {
+    botpressWebChat: Webchat
+  }
+}
+
 
 // const roboto = Roboto({weight: ['100', '300', '400'], subsets: ['vietnamese']});
 
