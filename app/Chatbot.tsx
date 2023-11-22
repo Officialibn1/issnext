b9b8ee44e0655fd72b1e719a035a76c21c6aa86b
ibn <officialibn001@gmail.com>
2023-11-22 14:21:12 +0100

    Fixed bot now showing

--- app/Chatbot.tsx
+++ app/Chatbot.tsx
@@ -1,14 +1,22 @@
-'use client'
-import Script from 'next/script'
-import React from 'react'
+/* eslint-disable @next/next/no-before-interactive-script-outside-document */
+"use client";
+import Script from "next/script";
+import React from "react";
 
 const Chatbot = () => {
-  return (
-    <div>
-      <Script src="https://cdn.botpress.cloud/webchat/v1/inject.js" />
-      <Script src="https://mediafiles.botpress.cloud/5b9adf49-934c-49b1-b05b-91b3d2d9fd8f/webchat/config.js" defer/>
-    </div>
-  )
-}
+	return (
+		<div>
+			<Script
+				src='https://cdn.botpress.cloud/webchat/v1/inject.js'
+				strategy='beforeInteractive'
+			/>
+			<Script
+				src='https://mediafiles.botpress.cloud/5b9adf49-934c-49b1-b05b-91b3d2d9fd8f/webchat/config.js'
+				strategy='beforeInteractive'
+				defer
+			/>
+		</div>
+	);
+};
 
-export default Chatbot
+export default Chatbot;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,71 +1,69 @@
-
-import '../styles/Globals.scss';
-import Chatbot from './Chatbot';
-import Footer from './Footer';
-import NavBar from './Navbar';
+import "../styles/Globals.scss";
+import Chatbot from "./Chatbot";
+import Footer from "./Footer";
+import NavBar from "./Navbar";
 declare global {
-  interface Webchat {
-    init: (options: any, id?: string) => void
-    sendEvent: (event: any, id?: string) => void
-    onEvent: (
-      callback: (event: WebchatEvent) => void,
-      topics: WebchatEventType[],
-      id?: string
-    ) => void
-  }
+	interface Webchat {
+		init: (options: any, id?: string) => void;
+		sendEvent: (event: any, id?: string) => void;
+		onEvent: (
+			callback: (event: WebchatEvent) => void,
+			topics: WebchatEventType[],
+			id?: string,
+		) => void;
+	}
 
-  type WebchatEventType =
-    | "LIFECYCLE.LOADED"
-    | "LIFECYCLE.READY"
-    | "UI.OPENED"
-    | "UI.CLOSED"
-    | "UI.RESIZE" // is this necessary ?
-    | "UI.SET-CLASS" // is this necessary ?
-    | "CONFIG.SET"
-    | "MESSAGE.SENT"
-    | "MESSAGE.RECEIVED"
-    | "MESSAGE.SELECTED"
-    | "USER.CONNECTED"
+	type WebchatEventType =
+		| "LIFECYCLE.LOADED"
+		| "LIFECYCLE.READY"
+		| "UI.OPENED"
+		| "UI.CLOSED"
+		| "UI.RESIZE" // is this necessary ?
+		| "UI.SET-CLASS" // is this necessary ?
+		| "CONFIG.SET"
+		| "MESSAGE.SENT"
+		| "MESSAGE.RECEIVED"
+		| "MESSAGE.SELECTED"
+		| "USER.CONNECTED";
 
-  interface WebchatEvent {
-    type: WebchatEventType
-    value: any
-    chatId: string
-  }
+	interface WebchatEvent {
+		type: WebchatEventType;
+		value: any;
+		chatId: string;
+	}
 
-  interface Window {
-    botpressWebChat: Webchat
-  }
+	interface Window {
+		botpressWebChat: Webchat;
+	}
 }
-
 
 // const roboto = Roboto({weight: ['100', '300', '400'], subsets: ['vietnamese']});
 
 export const metadata = {
-  title: 'ISS (Internet Service Studio)',
-  description: 'Designed by Ibn',
-  other: {
-    rel: 'stylesheet',
-    url: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css'
-  }
-}
+	title: "ISS (Internet Service Studio)",
+	description: "Designed by Ibn",
+	other: {
+		rel: "stylesheet",
+		url: "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css",
+	},
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      {/* <Head>
+	return (
+		<html lang='en'>
+			{/* <Head>
         <Link rel="stylesheet" href={'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css'} />
       </Head> */}
-      <body>
-        <NavBar />
-        {children}
-        <Footer />
-        <Chatbot />
-      </body>
-    </html>
-  )
+			<body>
+				<NavBar />
+				{children}
+				<Footer />
+				<Chatbot />
+			</body>
+		</html>
+	);
 }
